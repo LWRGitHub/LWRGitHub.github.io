55fb1e8206eeed51f5760a0b9ff012c8f7ea69b8
LWRGitHub <53380788+LWRGitHub@users.noreply.github.com>
2019-09-29 14:45:09 -0700

    Reorganized
    spacing got mess with during up load, but I repaired it.

--- css-js-project/js/main.js
+++ css-js-project/js/main.js
@@ -2,7 +2,6 @@ const shapes = ['circle', 'diamond', 'square', 'triangle'];
 
 window.addEventListener('DOMContentLoaded', (e) => {
   let presentation = document.querySelector('hp-presentation');
-
   presentation.onclick = handlePresentationClick;
   presentation.addEventListener('animationend', handleAnimationEnd, false);
 });
@@ -10,23 +9,18 @@ window.addEventListener('DOMContentLoaded', (e) => {
 handlePresentationClick = (e) => {
   let current = document.querySelector('hp-slide.active');
   let next = current.nextElementSibling;
-
   if (next) {
     current.classList.remove('active');
     next.classList.add('active');
-
     next.querySelectorAll('.match').forEach(function (el) {
       setTimeout(function () { el.classList.remove('match'); }, 0);
     });
-
     const aa = parseInt(next.getAttribute('data-autoadvance'));
-
     if (!isNaN(aa)) {
       setTimeout(function (e) {
         handlePresentationClick(e);
       }, aa);
     }
-
     const osa = next.getAttribute('data-onshow');
     if (osa) {
       window[osa]();
@@ -37,7 +31,6 @@ handlePresentationClick = (e) => {
 handleAnimationEnd = (e) => {
   const slide = e.target.closest('hp-slide');
   const aa = slide.getAttribute('data-autoadvance');
-
   if (aa == 'animationend' && slide.classList.contains('active')) {
     handlePresentationClick(e);
   }
@@ -45,7 +38,6 @@ handleAnimationEnd = (e) => {
 
 setLearnImage = (imageName) => {
   let img = document.querySelector('hp-slide.active hp-learn img');
-
   if (img){
     img.src = 'images/' + imageName + '.svg';
   }
@@ -54,7 +46,6 @@ setLearnImage = (imageName) => {
 showLearning = () => {
   const ii = Math.floor(Math.random() * shapes.length);
   setLearnImage(shapes[ii]);
-
   let slide = document.querySelector('hp-slide.active');
   slide.classList.remove('learn-yes');
   slide.classList.remove('learn-no');
@@ -63,11 +54,9 @@ showLearning = () => {
 
 startLearning = (learningDelay) => {
   showLearning();
-
   setTimeout( () => {
     if (learningDelay > 1.1) {
       showLearning();
-
       learningDelay = Math.pow(learningDelay, 1/1.05);
       startLearning(learningDelay);
     }
@@ -80,19 +69,14 @@ runLearningSequence = () => {
 
 animateSVGStep = () => {
   const slide = document.querySelector('hp-slide.active');
-
   let svgs = slide.querySelectorAll('svg');
-
   if (svgs[0].children.length > 0) {
     const el = svgs[0].children[0];
-
     if (el) {
       svgs[1].appendChild(el.parentNode.removeChild(el));
     }
-
     return true;
   }
-
   return false;
 }
 
