ae011c729ad43b532f9d504a59bc393d9cb16cc1
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-09-10 16:59:46 -0700

    Resume added to contact & updates on footer

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -11,7 +11,14 @@ const ContactInfo = () => (
             <li>GitHub: <a className="p-0" href="https://github.com/LWRGitHub">GitHub.com/LWRGitHub</a> </li>
             <li>LinkedIn: <a className="p-0" href="https://www.linkedin.com/in/logan-reynolds/">Linkedin.com/in/Logan-Reynolds</a></li>
         </ul>
-    </div>
+
+        {/* Resume */}
+        <a className="p-0" href="files/logan-reynolds-resume.pdf" >
+            <span>Resume:  </span>
+            <img src="images/about/file.png" id="resume-btn" alt="Blue computer folder file With see-through background."/>
+        </a>
+        {/* /Resume */}
+</div>
 );
 const GMap = () => (
     <div className="col-sm-6">
