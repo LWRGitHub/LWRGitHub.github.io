4c9c1fde1ba18c5a3153ef12255e0617d9a0438b
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-09-10 16:54:12 -0700

    resume added to footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => (
         <div className="col-sm-2">
             <a href="https://github.com/LWRGitHub">GitHub</a>
             <a href="https://www.linkedin.com/in/logan-reynolds/">Linkedin</a>
+            <a href="https://lwrgithub.github.io/files/logan-reynolds-resume.pdf">Resume</a>
         </div>
         <br />
         <p>Logan Reynolds Personal Portfolio 2020&copy;</p>
