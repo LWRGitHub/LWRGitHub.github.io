fdf446e3e46b08b2258c09d5de054ed3cb0d3195
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-09-10 16:55:37 -0700

    txt update on footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
     <footer className="container">
         <div className="col-sm-2">
             <a href="https://github.com/LWRGitHub">GitHub</a>
-            <a href="https://www.linkedin.com/in/logan-reynolds/">Linkedin</a>
+            <a href="https://www.linkedin.com/in/logan-reynolds/">LinkedIn</a>
             <a href="https://lwrgithub.github.io/files/logan-reynolds-resume.pdf">Resume</a>
         </div>
         <br />
