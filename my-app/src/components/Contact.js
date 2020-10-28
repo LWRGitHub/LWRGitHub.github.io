2ff759de225736cbd41440d3c0b6089b8abb03ef
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-10-27 18:58:06 -0700

    number

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -5,7 +5,7 @@ import Header from './Header';
 const ContactInfo = () => (
     <div className="col-sm-6">
         <ul>
-            <li>Cell: <a className="p-0" href='tel:5607411843'>‪(650) 741-1843</a></li>
+            <li>Cell: <a className="p-0" href='tel:4158860145'>‪(415) 886-0145</a></li>
             <li>Email: <a className="p-0" href="mailto:engineerlwr@gmail.com?subject=Seen%20Portfolio%20Site">EngineerLWR@gmail.com</a></li>
             <li>Current Location: Greater San Francisco City Area</li>
             <li>GitHub: <a className="p-0" href="https://github.com/LWRGitHub">GitHub.com/LWRGitHub</a> </li>
