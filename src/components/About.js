5ca92b15715674189e795b5afac897d4a496c52a
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-01-23 16:21:16 -0800

    About me pg Hello message edited to say Software Engr not CS + Resume skills simplified

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,9 @@ const MainContent = () => (
           {/* Self info & img */}
           <img className='rounded' src="images/about/me.jpeg" alt="Cartoon Sketch drawing image of computer programmer Software Engineer LWR."/>
           <p>Hello,</p>
-          <p>My name is Logan and I'm passionate about providing quality computer science service in the San Francisco area. I always go above and beyond to ensure I get the job done right the first time. My work stands out because I pay close attention to all details of the job.</p>
-          <p>I’ve been doing computer science work for a while now, and I love it because it gives me the chance to have fun with computer programing by designing applications that everybody loves.</p>
-          <p>I look forward to working with you to help meet all your computer science needs.</p>
+          <p>My name is Logan and I'm passionate about providing quality software engineering service in the San Francisco area. I always go above and beyond to ensure I get the job done right the first time. My work stands out because I pay close attention to all details of the job.</p>
+          <p>I’ve been doing software engineering work for a while now, and I love it because it gives me the chance to have fun with computer programing by designing applications that everybody loves.</p>
+          <p>I look forward to working with you to help meet all your software engineering needs.</p>
           {/* /Self info & img */}
 
           {/* Resume */}
