81f6c422bdf2a717c747e2177a183ff27177e067
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-07-18 16:31:22 -0700

    Separate paragraphs, about me description

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,8 @@ const MainContent = () => (
 
           {/* Self info & img */}
           <img className='rounded' src="images/about/me.jpeg" alt="Cartoon Sketch drawing image of computer programmer Software Engineer LWR."/>
-          <p>Logan is a Software engineer with a focus on BEW, with good Knowledge of FEW. He always goes above and beyond to ensure the job gets done right the first time. Logan's work stands out because he pays close attention to all details of the job. He's been doing computer science work for a while now, and loves it because it gives him the chance to have fun with computer programming by designing applications that everybody loves. During Logan's free time you can find him watching movies, coding his own projects, and having fun with friends!</p>
+          <p>Logan is a Software engineer with a focus on BEW, with good Knowledge of FEW. He always goes above and beyond to ensure the job gets done right the first time. His work stands out because he pays close attention to all details of the job. </p> 
+          <p>Logan's been doing computer science work for a while now, and loves it because it gives him the chance to have fun with computer programming by designing applications that everybody loves. During Logan's free time you can find him watching movies, coding his own projects, and having fun with friends!</p>
           {/* Old Message */}
           {/* <p>Hello,</p>
           <p>My name is Logan and I'm passionate about providing quality software engineering service in the San Francisco area. I always go above and beyond to ensure I get the job done right the first time. My work stands out because I pay close attention to all details of the job.</p>
