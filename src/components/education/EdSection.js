d92d32651c3c3ec04a55610e7758243e5ef4dce0
LWRGitHub <LoganReynoldsLWR@gmail.com>
2022-10-08 00:36:33 -0700

    Adobe job added

--- src/components/education/EdSection.js
+++ src/components/education/EdSection.js
@@ -6,13 +6,11 @@ const EdSection = (props) => {
     return (
         <div className="col-sm-6 p-3">
           <div className='card pb-4'>
-          <img src={props.src} alt={props.alt} />
+            <img src={props.src} alt={props.alt} />
             <p className="font-weight-bold card-title px-3 ">{props.title}</p>
-            
             <div className='pl-4'>
               <Schools data={props.schools} />
             </div>
-            
           </div>
         </div>
     );
