3a01c8105e482e8228567c845bffe37f5e9e98c9
LWRGitHub <LoganReynoldsLWR@gmail.com>
2024-08-23 19:58:39 -0700

    rm cert link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,6 @@ const Footer = () => (
         <div className="col">
             <a href="https://github.com/LWRGitHub">GitHub</a>
             <a href="https://www.linkedin.com/in/logan-reynolds/">LinkedIn</a>
-            <a href="files/education-credentials/ctd-certificate.pdf">Cert</a>
         </div>
 
         <br />
