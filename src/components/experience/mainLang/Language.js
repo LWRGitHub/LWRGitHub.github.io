67f5d12a8525b7a91c20512c6d952e357d1495d1
Logan Reynolds <loganreynolds@mac.mynetworksettings.com>
2026-03-04 15:50:04 -0800

    Add AWS/DevOps skills, update top skills layout

--- src/components/experience/mainLang/Language.js
+++ src/components/experience/mainLang/Language.js
@@ -3,11 +3,15 @@ import React from 'react';
 const Language = (props) => {
 
     return (
-        <div className="col-sm-6 col-md-3">
-
-            <h5>{props.id}</h5>
-            <img src={props.src} alt={props.alt} />
-
+        <div className="col-sm-6 col-md-3 mb-3">
+            <div className="card h-100" style={{border: 'none', backgroundColor: 'transparent'}}>
+                <div className="card-header" style={{border: 'none', backgroundColor: 'transparent'}}>
+                    <h5 className="mb-0">{props.id}</h5>
+                </div>
+                <div className="card-body d-flex align-items-center justify-content-center" style={{backgroundColor: 'transparent'}}>
+                    <img src={props.src} alt={props.alt} />
+                </div>
+            </div>
         </div>
     );
 }
--- src/data/experiencePgSkillSort.js
+++ src/data/experiencePgSkillSort.js
@@ -3,28 +3,23 @@ import {mainLang} from './skills';
 
 export const mainLanguages = [
     {
-        id: mainLang.js.id,
-        src: "images/experience/js-logo.svg",
-        alt: "programming language logo for JavaScript"
-    },
-    {
         id: mainLang.py.id,
         src: "images/experience/python-logo.svg",
         alt: "programming language logo for Python"
     },
     {
-        id: mainLang.go.id,
-        src: "images/experience/go-logo.webp",
-        alt: "programming language logo for Golang"
+        id: "DevOps",
+        src: "images/experience/devops.avif",
+        alt: "DevOps, Continuous Integration and Continuous Delivery (CI/CD) logo. A blue background with a white infinity symbol in the middle of the image."
     },
     {
-        id: mainLang.ts.id,
-        src: "images/experience/typescript_logo.svg",
-        alt: "programming language logo for TypeScript"
+        id: "Amazon Web Services (AWS)",
+        src: "images/experience/aws-logo.png",
+        alt: "Amazon Web Services (AWS) logo"
     },
     {
-        id: mainLang.java.id,
-        src: "images/experience/java-logo.webp",
-        alt: "programming language logo for Java"
+        id: mainLang.js.id,
+        src: "images/experience/js-logo.svg",
+        alt: "programming language logo for JavaScript"
     },
 ];
